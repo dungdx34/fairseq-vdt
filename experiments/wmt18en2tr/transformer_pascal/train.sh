2121e69550833f9f24094e79592aa10fadf3835a
phandat128 <phandat12082002@gmail.com>
2023-08-01 15:04:54 +0700

    upload

--- experiments/wmt18en2tr/transformer_pascal/train.sh
+++ experiments/wmt18en2tr/transformer_pascal/train.sh
@@ -4,9 +4,9 @@
 # Date created:       9/4/2019
 # Date last modified: 9/4/2019
 
-DATADIR=/home/hnc/PycharmProjects/fairseq-vdt/data/wmt18tren/corpus/wmt18en2tr
-TAGSDIR=/home/hnc/PycharmProjects/fairseq-vdt/data/wmt18tren/tags_mean/wmt18en2tr
-CKPTS=/home/hnc/PycharmProjects/fairseq-vdt/models
+DATADIR=./data/wmt18tren/corpus/wmt18en2tr
+TAGSDIR=./data/wmt18tren/tags_mean/wmt18en2tr
+CKPTS=./models
 #export CUDA_VISIBLE_DEVICES=0,1,2,3
 
 #NODES=$1
@@ -17,38 +17,43 @@ CKPTS=/home/hnc/PycharmProjects/fairseq-vdt/models
 #h=0
 #n=0
 
-params="$DATADIR \
-        --save-dir $CKPTS \
-	--tags-data $TAGSDIR \
-	--encoder-pascal-heads 7 0 0 0 0 0 \
-        --dropout 0.3 \
-	--parent-ignoring 0.3 \
-        --share-all-embeddings \
-        --optimizer adam \
-        --adam-betas (0.9,0.98) \
-	--adam-eps 1e-09 \
-        --clip-norm 0.0 \
-        --lr-scheduler inverse_sqrt \
-        --warmup-init-lr 1e-07 \
-        --warmup-updates 8000 \
-        --lr 0.0007 \
-        --min-lr 1e-09 \
-        --weight-decay 0.0 \
-        --criterion label_smoothed_cross_entropy \
-        --label-smoothing 0.1 \
-        --max-tokens 2048 \
-        --max-update 20000 \
-	--no-progress-bar \
-	--log-format json \
-	--log-interval 100 \
-	--save-interval 500000 \
-        --save-interval-updates 500 \
-	--keep-interval-updates 1 \
-	--arch pascal_transformer_wmt_en_de \
-	--task tags_translation
+params=".\data\wmt18tren\corpus\wmt18en2tr
+--save-dir .\models
+--tags-data .\data\wmt18tren\tags_mean\wmt18en2tr
+--encoder-pascal-heads
+7
+0
+0
+0
+0
+0
+--dropout 0.3
+--parent-ignoring 0.3
+--share-all-embeddings
+--optimizer adam
+--adam-betas (0.9,0.98)
+--adam-eps 1e-09
+--clip-norm 0.0
+--lr-scheduler inverse_sqrt
+--warmup-init-lr 1e-07
+--warmup-updates 8000
+--lr 0.0007
+--min-lr 1e-09
+--weight-decay 0.0
+--criterion label_smoothed_cross_entropy
+--label-smoothing 0.1
+--max-tokens 2048
+--max-update 20000
+--no-progress-bar
+--log-format json
+--log-interval 100
+--save-interval 500000
+--save-interval-updates 500
+--keep-interval-updates 1
+--arch pascal_transformer_wmt_en_de
+--task tags_translation
 "
 
-cd $HOME/pascal/fairseq
 mkdir -p $CKPTS
 
 #for line in $hosts; do
--- fairseq/models/pascal_transformer.py
+++ fairseq/models/pascal_transformer.py
@@ -436,7 +436,7 @@ class PascalTransformerEncoder(FairseqTagsEncoder):
 
         maxlen = src_tags.size(1) - 1
         src_tags = src_tags.detach().cpu().numpy()
-        parents = torch.cuda.FloatTensor(np.vectorize(lambda e: self.map_dictionary.get(e, maxlen))(src_tags))
+        parents = torch.FloatTensor(np.vectorize(lambda e: self.map_dictionary.get(e, maxlen))(src_tags))
 
         encoder_states = [] if return_all_hiddens else None
 
